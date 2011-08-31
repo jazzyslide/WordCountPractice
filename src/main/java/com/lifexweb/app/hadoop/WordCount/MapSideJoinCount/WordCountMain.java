b3481f179496eb89c0fb3f962cfdd152c4b1928e
Hideya Kato <jazzyslide@gmail.com>
2011-09-01 01:52:37 +0900

    MapSideJoinの実装。

---
+++ src/main/java/com/lifexweb/app/hadoop/WordCount/MapSideJoinCount/WordCountMain.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.lifexweb.app.hadoop.WordCount.MapSideJoinCount;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+/**
+ * @author kato_hideya
+ * WordCount(MapSideJoin)の実行用Mainクラス
+ */
+public class WordCountMain {
+
+	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException, URISyntaxException {
+		//設定情報の読み込み
+		Configuration conf = new Configuration();
+		
+		//DistributedCache
+		DistributedCache.addCacheFile(URI.create(args[2]), conf);
+		
+		//引数のパース
+		GenericOptionsParser parser = new GenericOptionsParser(conf, args);
+		args = parser.getRemainingArgs();
+		
+		Job job = new Job(conf, "WordCount MapSideJoin Ver.");
+		job.setJarByClass(WordCountMain.class);
+		
+		job.setMapperClass(WordCountMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setNumReduceTasks(1);
+		
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		boolean success = job.waitForCompletion(true);
+		if (success) System.out.println("Job SUCCESS!!!");
+	}
+
+}
---
+++ src/main/java/com/lifexweb/app/hadoop/WordCount/MapSideJoinCount/WordCountMapper.java
@@ -0,0 +1,101 @@
+package com.lifexweb.app.hadoop.WordCount.MapSideJoinCount;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	
+	private static final String PATTERN = "[-a-zA-Z0-9]+";
+    private Pattern pattern = Pattern.compile(PATTERN);
+    private Matcher matcher;
+ 
+    private Text resultKey = new Text();
+    private static final IntWritable ONE = new IntWritable(1);
+    private URI[] cacheFilePaths;
+    private HashMap<String,String> wordUrlPair = new HashMap<String, String>();
+    
+	private static Log log = LogFactory.getLog(WordCountMapper.class);
+
+	@Override
+	protected void setup(Context context) throws IOException,
+			InterruptedException {
+		super.setup(context);
+		log.info("[MAP:WordCountMapper] map task started... JobId: " + context.getJobID().getId());
+		
+		//DistributedCacheでキーワードとURLのマッピングをパース
+		wordUrlPair = parseCacheFile(context);
+	}
+    
+
+	@Override
+	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		String line = value.toString();
+
+		//正規表現でワードを抜き出すかたちに
+		matcher = pattern.matcher(line);
+		while (matcher.find()) {
+			for (String wordStr : matcher.group().split("\\s")){
+				if (wordUrlPair.get(wordStr) != null) {
+					resultKey.set(wordStr + "\t" + wordUrlPair.get(wordStr));
+					context.write(resultKey, ONE);
+				}
+			}
+		}
+	}
+	
+	@Override
+	protected void cleanup(Context context)
+			throws IOException, InterruptedException {
+		log.info("[MAP:WordCountMapper] map task finished... JobId: " + context.getJobID().getId());
+		super.cleanup(context);
+	}
+
+	
+	//DistributedCacheをパースしてHashMapに格納
+	private HashMap<String,String> parseCacheFile(Context context) throws IOException {
+		HashMap<String,String> retPair = new HashMap<String, String>();
+		cacheFilePaths = DistributedCache.getCacheFiles(context.getConfiguration());
+		
+		BufferedReader br = null;
+		try {
+			FileSystem fs = FileSystem.get(context.getConfiguration());
+			if (cacheFilePaths != null) {
+				for (URI uri : cacheFilePaths) {
+					log.info("[MAP:WordCountMapper]: cacheFile URI: " + uri.toString());
+					
+					br = new BufferedReader(new InputStreamReader(fs.open(new Path(uri))));
+					
+					String line = br.readLine();
+					while (line != null) {
+						String[] pair = line.split("\t");
+						if (pair.length == 2) {
+							retPair.put(pair[0], pair[1]);
+						}
+						line = br.readLine();
+					}
+				}
+			}
+		} finally {
+			if (br != null) {
+				br.close();	
+			}
+		}
+		return retPair;
+	}
+	
+}
